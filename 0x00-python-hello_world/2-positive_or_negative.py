0aaf0c5abeb63bfe4407eaef09ba2ccfd4b3d513
shaas6 <seanhaas@live.com>
2021-09-28 10:55:09 -0700

    Add: Task 2

---
+++ 0x00-python-hello_world/2-positive_or_negative.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+i = 0
+if number > i:
+    print(number,"is positive")
+if number == i:
+    print(number,"is zero")
+if number < i:
+    print(number,"is negative")

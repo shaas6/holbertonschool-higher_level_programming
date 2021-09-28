20e4240a2e37bcf8452717093e71c6fd2d9c80d0
shaas6 <seanhaas@live.com>
2021-09-28 10:56:42 -0700

    Edit: whitespace in print function calls

--- 0x00-python-hello_world/2-positive_or_negative.py
+++ 0x00-python-hello_world/2-positive_or_negative.py
@@ -3,8 +3,8 @@ import random
 number = random.randint(-10, 10)
 i = 0
 if number > i:
-    print(number,"is positive")
+    print(number, "is positive")
 if number == i:
-    print(number,"is zero")
+    print(number, "is zero")
 if number < i:
-    print(number,"is negative")
+    print(number, "is negative")

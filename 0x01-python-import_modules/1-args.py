017ea88703b5f0c5ce571bc3bcd0814967c0bd0c
shaas6 <seanhaas@live.com>
2021-09-29 18:17:29 -0700

    Pep8!

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -2,9 +2,9 @@
 if __name__ == "__main__":
     import sys
     print("{:d} argument{}{}"
-            .format(len(sys.argv) - 1,
-                's' if len(sys.argv) - 1 != 1 else '',
-                '.' if len(sys.argv) - 1 == 0 else ':'))
+        .format(len(sys.argv) - 1,
+            's' if len(sys.argv) - 1 != 1 else '',
+            '.' if len(sys.argv) - 1 == 0 else ':'))
     if len(sys.argv) - 1 > 0:
         usr_args = sys.argv[1:]
         i = 1

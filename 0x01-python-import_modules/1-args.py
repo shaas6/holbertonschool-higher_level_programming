2e1ffa01cd0b80c6a2397f505b0cf3d6f388bf6d
shaas6 <seanhaas@live.com>
2021-09-29 18:16:13 -0700

    Task 1 fix

---
+++ 0x01-python-import_modules/1-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    print("{:d} argument{}{}"
+            .format(len(sys.argv) - 1,
+                's' if len(sys.argv) - 1 != 1 else '',
+                '.' if len(sys.argv) - 1 == 0 else ':'))
+    if len(sys.argv) - 1 > 0:
+        usr_args = sys.argv[1:]
+        i = 1
+        for arg in usr_args:
+            print("{:d}: {}".format(i, arg))
+            i += 1

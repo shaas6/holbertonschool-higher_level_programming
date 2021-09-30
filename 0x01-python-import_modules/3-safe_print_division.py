6afc5b5cc0cea9f3da308288f6577f25d3942364
shaas6 <seanhaas@live.com>
2021-09-29 19:08:14 -0700

    Fixing spacing around !=

--- 0x01-python-import_modules/3-safe_print_division.py
+++ 0x01-python-import_modules/3-safe_print_division.py
@@ -5,7 +5,7 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         pass
     finally:
-        if b!= 0:
+        if b != 0:
             print("Inside result: {}".format(a/b))
             return a/b
         else:

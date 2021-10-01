1f073258b13d0f119c7c4e53a9a49b3faa9b36f8
shaas6 <seanhaas@live.com>
2021-09-30 19:01:38 -0700

    Task 5 fix

--- 0x02-python-data_structures/5-no_c.py
+++ 0x02-python-data_structures/5-no_c.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    i = 0
-    my_list = list(my_string)
-    for ch in my_list:
-        if ch == 'c' or ch == 'C':
-            del my_list[i]
-        i = i + 1
-    return ''.join(my_list)
+    c_less_str = "".join([char for char in my_string if char not in "Cc"])
+    return c_less_str

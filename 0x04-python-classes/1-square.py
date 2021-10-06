baa7aa4c02f180f9a44a849a74cb40854364d958
shaas6 <seanhaas@live.com>
2021-10-05 18:38:19 -0700

    Tasks 0 and 1, Readme add

---
+++ 0x04-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Square class with size attribute"""
+
+
+class Square:
+    """Class defines square"""
+    def __init__(self, size):
+        """Sets size"""
+        self.__size = size

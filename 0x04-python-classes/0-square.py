5b5a68773c8c3bafa1075892fd2f1788100f6712
shaas6 <seanhaas@live.com>
2021-10-05 18:57:11 -0700

    Task 0 fixes

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+# 0-square.py
 """Create square class"""
 
 
 class Square:
     """Square class definition"""
-    pass
+    def __init__(self, size):
+        self.__size = size

2107f7a73c5ad1c9ab576909c5607667b86601fc
shaas6 <seanhaas@live.com>
2021-10-13 19:18:51 -0700

    Missing parenthesis

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,6 +4,6 @@
 
 def inherits_from(obj, a_class):
     """Returns false if class and obj match"""
-    if type(obj is a_class:
+    if type(obj) is a_class:
         return False
     return (issubclass(type(obj), a_class))
